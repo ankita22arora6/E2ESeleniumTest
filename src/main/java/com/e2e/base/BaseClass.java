243716bf68dcea1872f006e617a72149b76b046e
ankita-test <ankita22.arora6@gmail.com>
2025-03-11 11:29:05 +0530

    working code 11 3

--- src/main/java/com/e2e/base/BaseClass.java
+++ src/main/java/com/e2e/base/BaseClass.java
@@ -3,13 +3,14 @@ package com.e2e.base;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.util.Properties;
 import java.util.concurrent.TimeUnit;
+import java.util.Properties;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 import com.e2e.util.TestUtility;
+
 
 public class BaseClass {
 	
